a1029679aa0ba2929a8f7731fbab5818440fb4a9
BBomers <bob.Bomers@student.graafschapcollege.nl>
2025-10-06 13:13:55 +0200

    web direct to activiteiten

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::middleware(['auth'])->group(function () {
 
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect()->route('activiteiten.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
